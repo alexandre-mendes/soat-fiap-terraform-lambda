4655053de91411e9cffc8cc4fa57ed14e6df7ad1
Alexandre Mendes da Costa <alexandremendes1005@gmail.com>
2025-05-31 12:30:03 -0300

    format tf

--- terraform/authenticator/lamda-auth.tf
+++ terraform/authenticator/lamda-auth.tf
@@ -1,15 +1,15 @@
 resource "aws_lambda_function" "authenticator_lambda" {
-  function_name = "LambdaAuthenticator"
-  filename      = "${path.module}/lambda-authenticator.zip"
-  handler       = "index.handler"
-  runtime       = "nodejs18.x"
+  function_name    = "LambdaAuthenticator"
+  filename         = "${path.module}/lambda-authenticator.zip"
+  handler          = "index.handler"
+  runtime          = "nodejs18.x"
   source_code_hash = filebase64sha256("${path.module}/lambda-authenticator.zip")
-  role          = var.existing_lambda_role_arn
+  role             = var.existing_lambda_role_arn
 
   environment {
     variables = {
-      JWT_SECRET     = var.jwt_secret
-      CLIENTS_TABLE  = var.clients_table_name
+      JWT_SECRET    = var.jwt_secret
+      CLIENTS_TABLE = var.clients_table_name
     }
   }
 }
@@ -28,10 +28,10 @@ resource "aws_lambda_permission" "apigw_invoke" {
 }
 
 resource "aws_apigatewayv2_integration" "lambda_integration" {
-  api_id             = aws_apigatewayv2_api.http_api.id
-  integration_type   = "AWS_PROXY"
-  integration_uri    = aws_lambda_function.authenticator_lambda.invoke_arn
-  integration_method = "POST"
+  api_id                 = aws_apigatewayv2_api.http_api.id
+  integration_type       = "AWS_PROXY"
+  integration_uri        = aws_lambda_function.authenticator_lambda.invoke_arn
+  integration_method     = "POST"
   payload_format_version = "2.0"
 }
 
--- terraform/authenticator/variables.tf
+++ terraform/authenticator/variables.tf
@@ -1,17 +1,17 @@
 variable "jwt_secret" {
   type        = string
   description = "JWT secret used to sign tokens"
-  default = "T8xXUW3GFo6gyUpzP5Mn0PkdrVXugBnQ"
+  default     = "T8xXUW3GFo6gyUpzP5Mn0PkdrVXugBnQ"
 }
 
 variable "clients_table_name" {
   type        = string
   description = "Name of the DynamoDB table for clients"
-  default = "clientes"
+  default     = "clientes"
 }
 
 variable "existing_lambda_role_arn" {
   type        = string
   description = "ARN da role já existente com permissões para DynamoDB e execução de Lambda"
-  default = "arn:aws:iam::590422439565:role/LabRole"
+  default     = "arn:aws:iam::590422439565:role/LabRole"
 }
